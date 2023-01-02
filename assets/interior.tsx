720222dfda3439fa0d75eea6a75d41c724c81db4
Josh Matthews <josh@joshmatthews.net>
2023-01-01 19:33:14 -0500

    Add tiled assets.

---
+++ assets/interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="interior" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="interiors.png" width="1536" height="512"/>
+</tileset>
